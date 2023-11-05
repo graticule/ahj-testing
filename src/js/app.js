011c9b123451d1d537b524e8484fdeadb1c52a6d
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-11-05 10:20:50 +0800

    change isValid

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import CardWidget from "./card-widget/card-widget";
+import isValid from "./validation";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
   const widget = new CardWidget(container);
   widget.bindToDOM();
+  isValid(1789372997);
 });
--- src/js/validation.js
+++ src/js/validation.js
@@ -1,3 +1,24 @@
-export default isValid(value) {
-  
+export default function isValid(value) {
+  const cardDigits = Array.from(value.toString()).map((el) => +el);
+  console.log(cardDigits);
+  const length = cardDigits.length;
+  console.log(length);
+  const parity = length % 2;
+  console.log(parity);
+  let sum = 0;
+  console.log(`${sum}`);
+  for (let i = 1; i++; i < length) {
+    console.log(i);
+    if (i % 2 !== parity) {
+      sum += cardDigits[i - 1];
+    } else if (cardDigits[i - 1] > 4) {
+      sum += 2 * cardDigits[i - 1] - 9;
+    } else {
+      sum += 2 * cardDigits[i - 1];
+    }
+  }
+  console.log(sum);
+  console.log(10 - (sum % 10));
+  console.log(cardDigits[length - 1]);
+  return cardDigits[length - 1] === 10 - (sum % 10);
 }
